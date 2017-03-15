c2980e1218d91dc31f3b70b202d9c48bb12e68f6
Mire96 <milos.m96@hotmail.com>
2017-03-15 15:38:49 +0100

    dodate tostring i equals metode u klase Knjiga i Autor

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -40,6 +40,52 @@ public class Knjiga {
 	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + autori + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autori == null) ? 0 : autori.hashCode());
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		result = prime * result + izdanje;
+		result = prime * result + ((izdavac == null) ? 0 : izdavac.hashCode());
+		result = prime * result + ((naslov == null) ? 0 : naslov.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		if (isbn != other.isbn)
+			return false;
+		if (izdanje != other.izdanje)
+			return false;
+		if (izdavac == null) {
+			if (other.izdavac != null)
+				return false;
+		} else if (!izdavac.equals(other.izdavac))
+			return false;
+		if (naslov == null) {
+			if (other.naslov != null)
+				return false;
+		} else if (!naslov.equals(other.naslov))
+			return false;
+		return true;
+	}
 	
 	
 	
