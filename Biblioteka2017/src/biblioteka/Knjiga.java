557d49adf2beb977e1b45fce2f59c7c5da67857e
Mire96 <milos.m96@hotmail.com>
2017-03-15 15:18:42 +0100

    uvedena klasa autor

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -1,11 +1,13 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 	private String naslov;
 	private long isbn;
 	private String izdavac;
 	private int izdanje;
-	private String autori;
+	private LinkedList<Autor> autori;
 	
 	
 	public String getNaslov() {
@@ -32,12 +34,13 @@ public class Knjiga {
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
-	public String getAutori() {
+	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
-	public void setAutori(String autori) {
+	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
+	
 	
 	
 }
